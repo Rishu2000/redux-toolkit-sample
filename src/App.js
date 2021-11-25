c437e27e1411a7e5f4d3fca4a39f9d9b7210e077
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-25 16:37:12 +0530

    Used useDispatch hook to do the actions.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import "./App.css"
+import { decrement, increment, incrementByNum } from './redux/counter';
 
 const App = () => {
 
 const {count} = useSelector((state) => state.counter);
+const dispatch = useDispatch();
 
   return (
     <div className="App">
       <div>Count is: {count}</div>
-      <button>Increment</button>
-      <button>Decerement</button>
-      <button>Increment by 100</button>
+      <button
+        onClick={() => dispatch(increment())}
+      >Increment</button>
+      <button
+        onClick={() => dispatch(decrement())}
+      >Decerement</button>
+      <button
+        onClick={() => dispatch(incrementByNum(100))}  
+      >Increment by 100</button>
     </div>
   )
 }
