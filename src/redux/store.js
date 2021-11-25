5a006e721e70780526041e0b1a3f987f537e7f44
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-25 15:47:15 +0530

    configureStore() automatically sets up the store with good default settings.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-export const store = configureStore({
+export const store = configureStore({   //configureStore() automatically sets up the store with good default settings
     reducer:{}          //configureStore() accepts a reducer function as a named argument
 })
